a4f9af7bd2af298c6fef2674681a720e14a142e8
Ryan Matthews <ryan.matthews.professional@gmail.com>
2019-09-11 21:58:55 -0400

    fix api endpoint exits

--- server.js
+++ server.js
@@ -72,7 +72,8 @@ server.get('/init',	(req,	res)	=>	{
 		headers: headers
 	}, (error, response, body)	=>	{
 		const data = JSON.parse(body)
-		res.status(200).json({data: data, exits: graph[data.room_id].exits})
+		currentRoom = data.room_id
+		res.status(200).json({data: data, exits: graph[currentRoom].exits})
 	})
 })
 
@@ -85,7 +86,8 @@ server.post('/move',	(req, res)	=>	{
 		body: `{"direction":"${req.body.dir}", "next_room_id": "${req.body.predict}"}`
 	}, (error, response, body)	=>	{
 		const data = JSON.parse(body)
-		res.status(200).json({data: data, exits: graph[data.room_id].exits})
+		currentRoom = data.room_id
+		res.status(200).json({data: data, exits: graph[currentRoom].exits})
 	})
 })
 
@@ -98,7 +100,7 @@ server.post('/take',	(req, res)	=>	{
 		body: `{"name":"${req.body.item}"}`
 	},	(error, response, body)	=>	{
 		const data = JSON.parse(body)
-		res.status(200).json({data: data, exits: graph[data.room_id].exits})
+		res.status(200).json({data: data, exits: graph[currentRoom].exits})
 	})
 })
 server.post('/drop',	(req, res)	=>	{
@@ -110,7 +112,7 @@ server.post('/drop',	(req, res)	=>	{
 		body: `{"name":"${req.body.item}"}`
 	},	(error, response, body)	=>	{
 		const data = JSON.parse(body)
-		res.status(200).json({data: data, exits: graph[data.room_id].exits})
+		res.status(200).json({data: data, exits: graph[currentRoom].exits})
 	})
 })
 server.post('/sell',	(req, res)	=>	{
@@ -122,7 +124,7 @@ server.post('/sell',	(req, res)	=>	{
 		body: `{"name":"${req.body.item}"}`
 	},	(error, response, body)	=>	{
 		const data = JSON.parse(body)
-		res.status(200).json({data: data, exits: graph[data.room_id].exits})
+		res.status(200).json({data: data, exits: graph[currentRoom].exits})
 	})
 })
 server.post('/sell/confirm',	(req, res)	=>	{
@@ -134,7 +136,7 @@ server.post('/sell/confirm',	(req, res)	=>	{
 		body: `{"name":"${req.body.item}", "confirm":"yes"}`
 	},	(error, response, body)	=>	{
 		const data = JSON.parse(body)
-		res.status(200).json({data: data, exits: graph[data.room_id].exits})
+		res.status(200).json({data: data, exits: graph[currentRoom].exits})
 	})
 })
 server.post('/status',	(req, res)	=>	{
@@ -145,7 +147,7 @@ server.post('/status',	(req, res)	=>	{
 		method: 'POST'
 	},	(error, response, body)	=>	{
 		const data = JSON.parse(body)
-		res.status(200).json({data: data, exits: graph[data.room_id].exits})
+		res.status(200).json({data: data, exits: graph[currentRoom].exits})
 	})
 })
 server.post('/changeName',	(req, res)	=>	{
@@ -158,7 +160,7 @@ server.post('/changeName',	(req, res)	=>	{
 	},	(error, response, body)	=>	{
 		console.log(error)
 		console.log(body)
-		res.status(200).json({data: body, exits: graph[data.room_id].exits})
+		res.status(200).json({data: body, exits: graph[currentRoom].exits})
 	})
 })
 
@@ -172,7 +174,7 @@ server.post('/changeName/confirm',	(req, res)	=>	{
 	},	(error, response, body)	=>	{
 		console.log(error)
 		console.log(body)
-		res.status(200).json({data: body, exits: graph[data.room_id].exits})
+		res.status(200).json({data: body, exits: graph[currentRoom].exits})
 	})
 })
 
